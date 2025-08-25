06c6f87e245765a939878672633ce5daee945de3
praveen-2730 <praveen27599@gmail.com>
2025-08-25 11:12:14 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="logo">DP</div>
+        <div className="logo">MPK</div>
         <div className={show ? "navLinks showmenu" : "navLinks"}>
           <div className="links">
             {data[0].navbarLinks.map((element) => (
